1fad6ca54c334ab5931c2f6bb78aeae0f11244f0
enue <otoriden@gmail.com>
2022-03-25 23:08:14 +0900

    Revert "wip spline"
    This reverts commit 7ab0980ff1b907f6ac914af650b0177941eae799.

--- Assets/ManipulateSample.cs
+++ Assets/ManipulateSample.cs
@@ -20,7 +20,7 @@ namespace TSKT
             y.Add(target.transform.position.y);
             z.Add(target.transform.position.z);
             t.Add(0f);
-            for (int i = 0; i < 100; ++i)
+            for (int i = 0; i < 2; ++i)
             {
                 x.Add(Random.Range(-5f, 5f));
                 y.Add(Random.Range(-5f, 5f));
@@ -30,42 +30,21 @@ namespace TSKT
             x.Add(target.transform.position.x);
             y.Add(target.transform.position.y);
             z.Add(target.transform.position.z);
-            t.Add(t.Count + 1);
+            t.Add(3);
 
-            var fx = new Spline(t.Zip(x, (t, x) => (t, x)).ToArray());
-            var fy = new Spline(t.Zip(y, (t, y) => (t, y)).ToArray());
-            var fz = new Spline(t.Zip(z, (t, z) => (t, z)).ToArray());
-
-            foreach (var it in t)
-            {
-                var a = it - 0.00001f;
-                var b = it + 0.00001f;
-                UnityEngine.Assertions.Assert.AreNotEqual(b, a);
-                UnityEngine.Assertions.Assert.AreApproximatelyEqual(fx.Velocity(a), fx.Velocity(b), 0.001f);
-            }
+            var fx = CubicFunction.Solve4Points((t[0], x[0]), (t[1], x[1]), (t[2], x[2]), (t[3], x[3]));
+            var fy = CubicFunction.Solve4Points((t[0], y[0]), (t[1], y[1]), (t[2], y[2]), (t[3], y[3]));
+            var fz = CubicFunction.Solve4Points((t[0], z[0]), (t[1], z[1]), (t[2], z[2]), (t[3], z[3]));
 
             Tween.Manipulate(target.transform, duration: (float)t[^1])
                 .Action((_time, target) =>
                 {
                     var p = new Vector3(
-                        fx.Evaluate(_time * fx.Duration),
-                        fy.Evaluate(_time * fy.Duration),
-                        fz.Evaluate(_time * fz.Duration));
+                        fx.Evaluate(_time * 3f),
+                        fy.Evaluate(_time * 3f),
+                        fz.Evaluate(_time * 3f));
                     target.position = p;
                 });
-        }
-
-        void Test()
-        {
-            var s = new Spline(
-                (time: 0f, value: 0f),
-                (time: 1f, value: 1f),
-                (time: 2f, value: 1f),
-                (time: 3f, value: 1f),
-                (time: 4f, value: 1f),
-                (time: 5f, value: 0f));
-            UnityEngine.Assertions.Assert.AreApproximatelyEqual(s.Evaluate(1f), 1f);
-            UnityEngine.Assertions.Assert.AreApproximatelyEqual(s.Velocity(1f), s.Velocity(1f - Mathf.Epsilon));
         }
     }
 }
